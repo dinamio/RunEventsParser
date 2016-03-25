b8343d1bd64cd8b56e3e2d6f3187291b0539bae9
Nik <Tolchielnikov@gmail.com>
2016-03-25 11:05:45 +0200

    [rp - 3]Generate parser for DneprRun(Spring 2016) (n.tolchelnikov)

--- src/main/java/com/runeventsparser/service/DneprRunParser.java
+++ src/main/java/com/runeventsparser/service/DneprRunParser.java
@@ -2,6 +2,8 @@ package com.runeventsparser.service;
 
 
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,24 +11,36 @@ import java.util.StringTokenizer;
 
 import com.google.gson.Gson;
 import com.runeventsparser.bom.*;
+import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 
 
 public class DneprRunParser {
 
-    public List<Result> parseSameOnManege (String url, String path) throws IOException {
-
-        Document doc;
+    public List<Result> parseSameOnManege (String pathJson) throws IOException {
+        String url ="http://dneprrun.dp.ua/sorevnovanija/te-zhe-na-manezhe-2016";
+        String pathOffline = "src/main/java/com/runeventsparser/Files/HtmlFiles/te-zhe-na-manezhe-2016.html";
+        Document doc = null;
         List<Result> resultList = new LinkedList<Result>();
-
-        doc = Jsoup.connect(url).get();
         CreateFile cf = new CreateFile();
-        cf.createFileForOfflineParser(url,path);
-        Elements resultRows = doc.select("table").get(1).select("tr").select("li");
 
+        if((new File(pathOffline).exists())) {
+            doc = Jsoup.parse(new File(pathOffline),"utf-8");
+        }
+        else {
+            try {
+                doc = Jsoup.connect(url).get();
+                cf.createFileForOfflineParser(url, pathOffline);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Elements resultRows = doc.select("table").get(1).select("tr").select("li");
         for (int i = 8; i < resultRows.size(); i++) {
             if (resultRows.get(i).text().indexOf(":") == -1) {
                 if (Character.isDigit(resultRows.get(i).text().charAt(0))){
@@ -39,7 +53,6 @@ public class DneprRunParser {
                 }
                 continue;
             }
-
             String[] word = resultRows.get(i).text().trim().split("\\s+");
 
             Result result = new Result();
@@ -54,7 +67,6 @@ public class DneprRunParser {
                 distance.setName("3000 метров");
                 distance.setLength(3.0);
             }
-
             Runner runner = new Runner();
             runner.setSurname(word[0]);
             runner.setName(word[1]);
@@ -65,6 +77,63 @@ public class DneprRunParser {
         }
         return resultList;
     }
+
+    public List<Result> parseSpringOnNose (String pathJson) throws IOException {
+        String url = "http://dneprrun.dp.ua/sorevnovanija/vesna-na-nosu-2016";
+        String pathXls = "src/main/java/com/runeventsparser/Files/XlsFiles/Spring.xls";
+        String pathOffline = "src/main/java/com/runeventsparser/Files/HtmlFiles/vesna-na-nosu-2016.html";
+        List<Result> resultList = new LinkedList<Result>();
+        Document docUrl = null;
+        HSSFRow row;
+        HSSFWorkbook excelBook = new HSSFWorkbook(new FileInputStream(pathXls));
+
+        for (int i = 1; i <= 75; i++) {
+            row = excelBook.getSheetAt(0).getRow(i);
+
+            Result result = new Result();
+
+            result.setTime(setTime(row.getCell(3).getStringCellValue()));
+
+            Distance distance = new Distance();
+            distance.setLength(Double.valueOf(row.getCell(1).getStringCellValue().substring(0, row.getCell(1).getStringCellValue().length() - 1)) / 1000);
+            distance.setName(row.getCell(1).getStringCellValue());
+            result.setDistance(distance);
+
+            Runner runner = new Runner();
+            runner.setSurname(row.getCell(2).getStringCellValue().substring(0, row.getCell(2).getStringCellValue().lastIndexOf(" ")));
+            runner.setName(row.getCell(2).getStringCellValue().substring(row.getCell(2).getStringCellValue().substring(0, row.getCell(2).getStringCellValue().lastIndexOf(" ")).length()).trim());
+            result.setRunner(runner);
+
+            if (row.getCell(5) == null) {
+                runner.setSex(Sex.FEMALE);
+            } else runner.setSex(Sex.MALE);
+            resultList.add(result);
+        }
+        excelBook.close();
+
+        CreateFile cf = new CreateFile();
+        if ((new File(pathOffline).exists())) {
+            docUrl = Jsoup.parse(new File(pathOffline), "utf-8");
+        } else {
+            try {
+                docUrl = Jsoup.connect(url).get();
+                cf.createFileForOfflineParser(url, pathOffline);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Elements resultRows = docUrl.select("table").get(1).select("tr").select("li");
+        for (int i = 0; i < resultRows.size(); i++) {
+            String[] wordNumb = resultRows.get(i).text().trim().split("\\s+");
+            for (int j = 0; j < resultList.size(); j++) {
+                if (wordNumb[1].equals(resultList.get(j).getRunner().getSurname()) && wordNumb[2].equals(resultList.get(j).getRunner().getName())) {
+                    resultList.get(j).setNumber(wordNumb[0]);
+                }
+            }
+        }
+            return resultList;
+        }
 
     public Time setTime (String st){
         Time time = new Time();
@@ -79,7 +148,7 @@ public class DneprRunParser {
         return time;
 }
 
-    public String parseToJson(String url,String path) throws IOException {
-        return new Gson().toJson(parseSameOnManege(url,path));
+    public String parseToJson(List<Result> resultList) throws IOException {
+        return new Gson().toJson(resultList);
     }
 }
---
+++ src/test/java/ConvertDneprRunTest.java
@@ -0,0 +1,52 @@
+import com.runeventsparser.bom.Result;
+import com.runeventsparser.service.Converter;
+import com.runeventsparser.service.DneprRunParser;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * Created by zOpa on 24.03.2016.
+ */
+public class ConvertDneprRunTest {
+    DneprRunParser dneprRun = new DneprRunParser();
+    String pathJsonSameOnManege = "src/main/java/com/runeventsparser/Files/JsonFiles/SameOnManege.json";
+    String pathJsonSpringOnNose = "src/main/java/com/runeventsparser/Files/JsonFiles/SpringOnNose.json";
+
+    @Test
+    public void testRun() throws IOException {
+        parseDneprRunTest(pathJsonSameOnManege,dneprRun.parseSameOnManege(pathJsonSameOnManege));
+        parseDneprRunTest(pathJsonSpringOnNose,dneprRun.parseSpringOnNose(pathJsonSpringOnNose));
+    }
+
+    public void parseDneprRunTest(String jsonFile,List<Result> dneprRun) throws IOException {
+        DneprRunParser dneprRunParser = new DneprRunParser();
+
+        Converter converter = new Converter();
+        List<Result> listFromJson = new ArrayList<Result>();
+        List<Result> resultList = null;
+        String json = null;
+
+        json = dneprRunParser.parseToJson(dneprRun);
+        resultList = dneprRun;
+        converter.convertToJson(jsonFile,json);
+        listFromJson = converter.convertToEntity(jsonFile);
+
+        assertNotNull(new File(jsonFile));
+        assertEquals(resultList.size(), listFromJson.size());
+        assertEquals(resultList.get(0).getRunner().getName(),listFromJson.get(0).getRunner().getName());
+        assertEquals(resultList.get(0).getRunner().getSurname(),listFromJson.get(0).getRunner().getSurname());
+        assertEquals(resultList.get(0).getDistance().getName(),listFromJson.get(0).getDistance().getName());
+        assertEquals(resultList.get(0).getDistance().getLength(),listFromJson.get(0).getDistance().getLength());
+        assertEquals(resultList.get(0).getNumber(),listFromJson.get(0).getNumber());
+        assertEquals(resultList.get(0).getRace(),listFromJson.get(0).getRace());
+        assertEquals(resultList.get(0).getTime(),listFromJson.get(0).getTime());
+        assertEquals(resultList.get(0).getRunner().getSex(),listFromJson.get(0).getRunner().getSex());
+    }
+}
